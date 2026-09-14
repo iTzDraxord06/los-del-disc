17ee969437f70f45822371c016ab75fef0dd5f0e
iTzDraxord_06 <u24239989@utp.edu.pe>
2026-09-14 01:27:19 -0500

    feat: refactor modular a multi-paginas, nuevo tema neon y directorio central de miembros

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ process.on('unhandledRejection', (err) => console.error('PROMESA NO CONTROLADA:'
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 // 1. Configuración de Cloudinary
 cloudinary.config({
@@ -33,7 +33,7 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 15 * 1024 * 1024 }
+    limits: { fileSize: 25 * 1024 * 1024 }
 });
 
 const manejarSubida = (req, res, next) => {
